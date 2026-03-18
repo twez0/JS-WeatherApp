c815ec7d8904220cd668911d5eb8a96a6c345c82
Vladislav <46641501+twez0@users.noreply.github.com>
2026-03-18 16:59:00 +0700

    final

--- src/js/6.js
+++
@@ -1,8 +0,0 @@
-const stringDate = new Date(1690297200 * 1000);
-const date = new Date();
-date.setHours(date.getHours() + -14400 / 3600);
-stringDate.setHours(stringDate.getHours() + -14400 / 3600);
-
-console.log(stringDate);
-//console.log(date.toISOString() <= stringDate.toISOString());
-
--- src/js/api.js
+++ src/js/api.js
@@ -1,20 +1,23 @@
 export const getWeatherData = async (city) => {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=834cd6f9c2b84522ccd4f8fed412f943&lang=en&units=metric`);
+	try {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=834cd6f9c2b84522ccd4f8fed412f943&lang=en&units=metric`
+		);
 
-        return await response.json();
-    } catch(error) {
-        console.error();
-    }
-}
+		return await response.json();
+	} catch (error) {
+		console.error();
+	}
+};
 
 export const getWeatherForDaysData = async (city) => {
-    try {
-        
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=6&appid=834cd6f9c2b84522ccd4f8fed412f943&lang=en&units=metric`);
+	try {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=6&appid=834cd6f9c2b84522ccd4f8fed412f943&lang=en&units=metric`
+		);
 
-        return await response.json();
-    } catch(error) {
-        console.error();
-    }
-}
+		return await response.json();
+	} catch (error) {
+		console.error();
+	}
+};
